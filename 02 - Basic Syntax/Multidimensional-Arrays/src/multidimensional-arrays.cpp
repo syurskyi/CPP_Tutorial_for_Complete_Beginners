f7f97a10ab0098398ab0228bad30d21898e1bfa5
Grey <sergejyurskyj@yahoo.com>
2020-07-07 18:05:03 -0700

    Committed 02 - Basic Syntax multidimensional-arrays

---
+++ 02 - Basic Syntax/Multidimensional-Arrays/src/multidimensional-arrays.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : break-continue.cpp
+// Author      : Serhii Yurskyi
+// Version     :
+//============================================================================
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string animals[][3] = {
+			{"fox", "dog", "cat"},
+			{"mouse", "squirrel", "parrot"}
+	};
+
+	for(int i=0; i<2; i++) {
+		for(int j=0; j<3; j++) {
+			cout << animals[i][j] << " " << flush;
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
