0d8e83861f429b497d2dc4994892917f70b04f2b
Grey <sergejyurskyj@yahoo.com>
2020-07-07 18:10:25 -0700

    Committed 03 - Subroutines - Reusable Blocks of Code, Functions Parameters

---
+++ 03 - Subroutines - Reusable Blocks of Code/Function-Parameters/function parameters/src/functions.cpp
@@ -0,0 +1,48 @@
+//============================================================================
+// Name        : break-continue.cpp
+// Author      : Serhii Yurskyi
+// Version     :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+	cout << "1. Search" << endl;
+	cout << "2. View Record" << endl;
+	cout << "3. Quit" << endl;
+}
+
+int getInput() {
+	cout << "Enter selection: " << endl;
+
+	int input;
+	cin >> input;
+
+	return input;
+}
+
+void processSelection(int option) {
+	switch (option) {
+	case 1:
+		cout << "Searching ..." << endl;
+		break;
+	case 2:
+		cout << "Viewing ..." << endl;
+		break;
+	case 3:
+		cout << "Quitting ..." << endl;
+		break;
+	default:
+		cout << "Please select an item from the menu." << endl;
+	}
+}
+
+int main() {
+
+	showMenu();
+	int selection = getInput();
+	processSelection(selection);
+
+	return 0;
+}
